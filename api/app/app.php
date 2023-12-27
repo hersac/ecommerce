8fb4089f7402a744db06944a96090a3c5b209f28
Heri <hersac1@gmail.com>
2023-12-27 14:04:56 -0500

    Se ajusta archivo .htaccess para generacion de endpoints

--- api/app/app.php
+++ api/app/app.php
@@ -2,43 +2,13 @@
 
 use app\routes\ProductosRoute;
 
-$baseURL = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-$prodRoute = null;
+$prodRoute = new ProductosRoute();
 
-// Define el patrón de URL esperado
-$urlPattern = '/^\/api\/productos\/?$/';
+if ($_SERVER['REQUEST_URI'] === '/api/products') {
 
-// Verifica si la URL coincide con el patrón
-if (preg_match($urlPattern, $baseURL)) {
-    // La URL coincide con el patrón, crea la instancia de ProductosRoute
-    $prodRoute = new ProductosRoute();
-}
+    header('Content-Type: application/json');
 
-if ($prodRoute) {
-    // La instancia de ProductosRoute se creó correctamente, realiza acciones según el método HTTP
-    switch ($method) {
-        case 'GET':
-            echo $prodRoute->getProducts();
-            break;
-        
-        case 'POST':
-            // Código para el método POST
-            break;
-        
-        case 'PUT':
-            // Código para el método PUT
-            break;
-        
-        case 'DELETE':
-            // Código para el método DELETE
-            break;
-        
-        default:
-            // Código para otros métodos
-            break;
-    }
-} else {
-    // La URL no coincide con el patrón, puedes manejar esto de acuerdo a tus necesidades
-    echo "URL no válida";
+    echo $prodRoute->getProducts();
+
+    exit();
 }
