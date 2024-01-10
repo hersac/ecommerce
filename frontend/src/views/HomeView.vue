e89ceeba585aa3d5916ed7092098501ef859bf02
Heri <hersac1@gmail.com>
2024-01-10 17:43:40 -0500

    Se trabaja sobre al vista Home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h2>HOME</h2>
+  <div class="p-4 h-screen">
+      <div class="w-full h-90 bg-white p-6 rounded-md shadow-md">
+        <h2 class="text-2xl uppercase">Home</h2>
+      </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 
 </script>
+
+<style type="text/css">
+  
+</style>
