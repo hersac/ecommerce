1e06930c2f4f43e06341648252680f881db23525
Heri <heriberto@viumail.com>
2024-03-12 23:28:48 -0500

    Se corrije ruta de detalles de carrito de compras

--- backend/src/main/java/com/hersac/backend/controllers/DetallesCCController.java
+++ backend/src/main/java/com/hersac/backend/controllers/DetallesCCController.java
@@ -13,39 +13,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hersac.backend.models.DetallesOrdenes;
-import com.hersac.backend.services.DetallesOrdenesService;
+import com.hersac.backend.models.DetallesCC;
+import com.hersac.backend.services.DetallesCCService;
 
 @RestController
-@RequestMapping("/api/detallesOrdenes")
+@RequestMapping("/api/detallesCC")
 public class DetallesCCController {
 
     @Autowired
-    private DetallesOrdenesService detallesOrdenesServ;
+    private DetallesCCService detallesCCService;
 
     @GetMapping
-    public Optional<List<DetallesOrdenes>> getDetallesOrdenes(){
-        return detallesOrdenesServ.getDetallesOrdenes();
+    public Optional<List<DetallesCC>> getDetallesOrdenes(){
+        return detallesCCService.getDetallesCC();
     }
 
     @GetMapping("/{id}")
-    public Optional<DetallesOrdenes> getDetallesOrdenesById(@PathVariable Long id){
-        return detallesOrdenesServ.getDetallesOrdenesById(id);
+    public Optional<DetallesCC> getDetallesOrdenesById(@PathVariable Long id){
+        return detallesCCService.getDetallesCCById(id);
     }
 
     @PostMapping
-    public Optional<String> addDetallesOrdenes(@RequestBody DetallesOrdenes newDetallesOrdenes){
-        return detallesOrdenesServ.addDetallesOrdenes(newDetallesOrdenes);
+    public Optional<String> addDetallesOrdenes(@RequestBody DetallesCC newDetallesOrdenes){
+        return detallesCCService.addDetallesCC(newDetallesOrdenes);
     }
 
     @PutMapping("/{id}")
-    public Optional<String> updateDetallesOrdenes(@PathVariable Long id, @RequestBody DetallesOrdenes newData){
-        return detallesOrdenesServ.updateDetallesOrdenes(id, newData);
+    public Optional<String> updateDetallesOrdenes(@PathVariable Long id, @RequestBody DetallesCC newData){
+        return detallesCCService.updateDetallesCC(id, newData);
     }
 
     @DeleteMapping("/{id}")
     public Optional<String> deleteDetallesOrdenes(@PathVariable Long id){
-        return detallesOrdenesServ.deleteDetallesOrdenesById(id);
+        return detallesCCService.deleteDetallesCCById(id);
     }
 
 }
