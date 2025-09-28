17727e252c676a0d388506be11d8823951ae37af
Heriberto Sanchez <hersac1@gmail.com>
2025-09-27 22:16:34 -0500

    Elimina proyecto api

--- api/app/interfaces/RouterInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\interfaces;
-
-interface RouterInterface {
-	public function handlerRoutes();
-}
--- api/app/models/Roles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace app\models;
-
-class Roles {
-	private int $rolId;
-	private string $nombreRol;
-}
